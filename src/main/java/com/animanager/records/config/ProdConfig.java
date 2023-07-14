bf8501272313402c33e6a7604acc8fd01be31d77
Gabriel Mediotti <medzdeveloper@gmail.com>
2023-07-14 13:56:00 -0300

    fix: Implemented the "prod" profile. Now it's possible to use the service in a Postgresql database.

---
+++ src/main/java/com/animanager/records/config/ProdConfig.java
@@ -0,0 +1,25 @@
+package com.animanager.records.config;
+
+import com.animanager.records.entities.InformationRecords;
+import com.animanager.records.entities.Role;
+import com.animanager.records.repositories.InformationRecordsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig implements CommandLineRunner {
+    @Autowired
+    private InformationRecordsRepository informationRecordsRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        InformationRecords ir1 = new InformationRecords(null, Role.CAREGIVER, "Gabriel", "Mediotti", "11999999999", "99999999999", "99999999", "845, ap92");
+        InformationRecords ir2 = new InformationRecords(null, Role.EMPLOYEE, "Giulia", "Pugliesi", "11999999999", "89999999999", "99999999", "81");
+        informationRecordsRepository.saveAll(Arrays.asList(ir1, ir2));
+    }
+}
--- src/main/java/com/animanager/records/entities/InformationRecords.java
+++ src/main/java/com/animanager/records/entities/InformationRecords.java
@@ -9,8 +9,8 @@ import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
-@Table(name = "information_records")
-@Entity(name = "information_records")
+@Table(name="information_records")
+@Entity
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
